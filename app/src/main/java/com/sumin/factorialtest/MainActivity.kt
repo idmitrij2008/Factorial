a9e34366421b0b5fb5a19af7dcf1bf38a3dcdf2d
Журба Дмитрий <dmitriy.zhurba@gazprombank.ru>
2022-08-13 12:33:02 +0300

    lesson_01

--- app/src/main/java/com/sumin/factorialtest/MainActivity.kt
+++ app/src/main/java/com/sumin/factorialtest/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.sumin.factorialtest
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.lifecycle.ViewModelProvider
 import com.sumin.factorialtest.databinding.ActivityMainBinding
 
 
@@ -11,8 +14,34 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private val viewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        observeViewModel()
+        binding.buttonCalculate.setOnClickListener {
+            viewModel.calculate(binding.editTextNumber.text.toString())
+        }
+    }
+
+    private fun observeViewModel() {
+        viewModel.progress.observe(this) {
+            binding.progressBarLoading.isVisible = it
+            binding.buttonCalculate.isEnabled = !it
+        }
+
+        viewModel.error.observe(this) {
+            if (it) {
+                Toast.makeText(this, "You did not enter value", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        viewModel.factorial.observe(this) {
+            binding.textViewFactorial.text = it
+        }
+
     }
 }
