8cdd80a0c17e7ebedb5805b9dff8e28e289945c9
dmitriiTT <zhurba@trend.tech>
2022-08-15 16:30:37 +0300

    lesson_05

--- app/src/main/java/com/sumin/factorialtest/MainViewModel.kt
+++ app/src/main/java/com/sumin/factorialtest/MainViewModel.kt
@@ -3,19 +3,22 @@ package com.sumin.factorialtest
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import java.math.BigInteger
-import kotlin.concurrent.thread
-import kotlin.coroutines.suspendCoroutine
 
 class MainViewModel : ViewModel() {
+
+    private val myCoroutineScope =
+        CoroutineScope(Dispatchers.Main + CoroutineName("myCoroutineScope"))
 
     private val _state = MutableLiveData<State>()
     val state: LiveData<State>
         get() = _state
+
+    override fun onCleared() {
+        super.onCleared()
+        myCoroutineScope.cancel()
+    }
 
     fun calculate(value: String?) {
         _state.value = Progress
@@ -25,34 +28,34 @@ class MainViewModel : ViewModel() {
             return
         }
 
-        viewModelScope.launch {
+        myCoroutineScope.launch {
             val number = value.toLong()
             val res = factorial(number)
             _state.value = Factorial(res.toString())
         }
     }
 
-//    private suspend fun factorial(number: Long) = withContext(Dispatchers.Default) {
-//        var res = BigInteger.ONE
-//
-//        for (i in 1..number) {
-//            res = res.multiply(BigInteger.valueOf(i))
-//        }
-//
-//        res.toString()
-//    }
+    private suspend fun factorial(number: Long) = withContext(Dispatchers.Default) {
+        var res = BigInteger.ONE
 
-    private suspend fun factorial(number: Long): String {
-        return suspendCoroutine {
-            thread {
-                var res = BigInteger.ONE
-
-                for (i in 1..number) {
-                    res = res.multiply(BigInteger.valueOf(i))
-                }
-
-                it.resumeWith(Result.success(res.toString()))
-            }
+        for (i in 1..number) {
+            res = res.multiply(BigInteger.valueOf(i))
         }
+
+        res.toString()
     }
+
+//    private suspend fun factorial(number: Long): String {
+//        return suspendCoroutine {
+//            thread {
+//                var res = BigInteger.ONE
+//
+//                for (i in 1..number) {
+//                    res = res.multiply(BigInteger.valueOf(i))
+//                }
+//
+//                it.resumeWith(Result.success(res.toString()))
+//            }
+//        }
+//    }
 }
